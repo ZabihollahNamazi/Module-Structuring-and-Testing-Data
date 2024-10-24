8ae8cc5866a1753064313d3141cf597161a3ae73
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-10-24 22:24:27 +0100

    Sprint1-Exercises-Paths js- made the dir part and ext part of the the filePath

--- Sprint-1/exercises/paths.js
+++ Sprint-1/exercises/paths.js
@@ -12,7 +12,12 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
+const dir = filePath.slice(0, lastSlashIndex);
+const lastDotIndex = base.lastIndexOf(".");
+const ext = base.slice(lastDotIndex + 1);
+console.log(`The dir part of the ${filePath} is ${dir}`);
 console.log(`The base part of ${filePath} is ${base}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
