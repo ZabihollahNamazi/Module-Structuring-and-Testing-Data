5b82f985a8ded5ed91ab4c2af9d24098afced641
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-01 16:11:55 +0000

    answered

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -1,8 +1,8 @@
 let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
-carPrice = Number(carPrice.replace(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replace(",", ""));
+carPrice = Number(carPrice.replaceAll(",", ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -22,13 +22,13 @@ console.log(`The percentage change is ${percentageChange}`);
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
 
 
-// a) 1-carPrice = Number(carPrice.replaceAll(",", ""));   2-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+// a) 1-carPrice = Number(carPrice.replace(",", ""));   2-priceAfterOneYear = Number(priceAfterOneYear.replace("," ""));
 //       3- console.log(`The percentage change is ${percentageChange}`);
 
 // b) there should be a ',' between the argument and the replacement
-// replaceAll is not a function, you should use replace instead
+// comma was missing
 
-// c) 1- carPrice   2-priceDifference
+// c) 1- carPrice   2-priceAfterOneYear
 
 // d) 1-carPrice   2-priceDifference   3-priceDifference   4-percentageChange
 
