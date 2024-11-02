8d1e1525cab8503b6d5b18d225e12e98c7066826
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-02 14:28:26 +0000

    sprint2-errors-0.js

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -4,6 +4,12 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+console.log(capitalise("hello"));
+
+// it gives an error "str has already been declared" so we don't need to define it again in line 7
+// in line 6 - we give the function the value or variable so in line 7- we don't nee to define the variable 
+// I removed the "let" before "str" in line 7
