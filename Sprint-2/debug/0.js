c4749bf40f5857c31c0cfe7a93bf48a71ed6a486
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-02 14:07:27 +0000

    sprint2-debug-0.js

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -1,7 +1,11 @@
 // Predict and explain first...
 
 function multiply(a, b) {
-  console.log(a * b);
+  return(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// first it will show you the result of first console.log and  in the second console.log , 
+// it'll show you the text and result og the function which is "undefined"
+// when we define the function , instead of "console.log" we have to use the "return" to get the value
