3445e882321fb0735400ff721483454a921884c0
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-02 15:04:36 +0000

    sprint2-extend-format-time.js

--- Sprint-2/extend/format-time.js
+++ Sprint-2/extend/format-time.js
@@ -4,13 +4,14 @@
 function formatAs12HourClock(time) {
   const hours = Number(time.slice(0, 2));
   if (hours > 12) {
-    return `${hours - 12}:00 pm`;
+    let x = `${hours - 12}`.padStart(2,"0");
+    return x+`:00 pm`;
   }
   return `${time} am`;
 }
 
-const currentOutput = formatAs12HourClock("08:00");
-const targetOutput = "08:00 am";
+const currentOutput = formatAs12HourClock("20:00");
+const targetOutput = "08:00 pm";
 console.assert(
   currentOutput === targetOutput,
   `current output: ${currentOutput}, target output: ${targetOutput}`
@@ -22,3 +23,7 @@ console.assert(
   currentOutput2 === targetOutput2,
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
+
+// it doesnt work when the giving number is more than 12 and we minus it by 12 it will be one single number
+// for example the output will be 8:00 pm but it must be 08:00 pm
+// so when the we do the minus then we add a 0 to start o the number
