9819f1115ac5301f635e49189fc2d29c397546c8
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-02 14:36:58 +0000

    sprint2-errors-1.js

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,14 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.6));
+
+// gives error tah decimalNumber has already been declared
+// we have to call the function instead of decimalNumber - the we give a value to function for decimalNumber
+// and we have to remove the line => "const decimalNumber = 0.5;" because
+// if we don't remove it , the function always works on 0.5 and result always will be 50%
