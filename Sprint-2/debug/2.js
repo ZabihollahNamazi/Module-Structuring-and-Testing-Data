2c74fb9a8544b7d92d3bd9ba80d384011e9fd783
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-08 14:11:54 +0000

    sprint2-debug- 2 js

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -2,8 +2,8 @@
 
 const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
@@ -12,3 +12,7 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+// it will show "3" for all each number. 
+// because we defined num variable and gave it a number and each time the function is using that number
+// we have to use a variable which we give the number each time and the function can use it
