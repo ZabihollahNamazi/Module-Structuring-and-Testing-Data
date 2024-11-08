8fef405ebefb677351cdaf5ecc03c4115c3ac612
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-08 14:10:45 +0000

    sprint2-debug- 1 js

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// teh result will be the text and "undefined" 
+// because we returned the function without giving it functionality, (a+b) should be infront of return
