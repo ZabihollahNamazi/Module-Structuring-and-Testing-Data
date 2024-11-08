10e6a5ecd15e11474af392e59fd127538197c43d
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-08 13:59:51 +0000

    sprint2-implement- to pound js

--- Sprint-2/implement/to-pounds.js
+++ Sprint-2/implement/to-pounds.js
@@ -4,3 +4,9 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+function toPounds(pence){
+    return `£${(pence / 100).toFixed(2)}`;
+} 
+
+console.log(toPounds(1554));
