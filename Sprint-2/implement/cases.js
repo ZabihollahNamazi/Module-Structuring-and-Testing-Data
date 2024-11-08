7eea3c6dc6fcc06e4165c738181ab0ab7169b4df
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-08 13:58:18 +0000

    sprint2-implement-cases js

--- Sprint-2/implement/cases.js
+++ Sprint-2/implement/cases.js
@@ -13,3 +13,21 @@
 
 // You will need to come up with an appropriate name for the function
 // Use the string documentation to help you find a solution
+
+var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lows = ["a", "b", "c", "d", "e", "f", "g", "h", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+function low_to_cap(txt){
+    let up_word = "";
+    for (let i =0; i < txt.length ; i++){
+        if (txt[i] == " "){
+            up_word += "_"
+        }
+        for (let j = 0; j < lows.length; j++){
+            if (txt[i] == lows[j]){
+               up_word += caps[j]; 
+            }
+        }
+    } return up_word;
+}
+
+console.log(low_to_cap("lord of the ring"));
