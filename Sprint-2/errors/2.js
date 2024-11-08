53d20cf8d85afcfb7ff0cbeec9e17726398c03a4
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-08 14:08:24 +0000

    sprint2-errors- 2 js

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,13 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
 
+console.log(square(4));
+
+// unexpected number
+// we have a number given to function but its working on different variable
+// we can give a number to function when we call the function and use it as a variable to work on it inside function "num"
