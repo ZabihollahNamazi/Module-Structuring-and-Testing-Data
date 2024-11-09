5825d6a8c87f8961cd71ed2744b8a754d5cb3cb0
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-09 16:05:34 +0000

    sprint3-revise-implement-is prime test js

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,16 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+function isPrime(num){
+    if (num <= 1) return false; 
+    else if (num <= 3) return true;
+    else if(num % 2 == 0 || num % 3 == 0){
+        return false;
+    }
+    else if (num % 1 == 0 && num % num == 0){
+        return true
+    }
+}
+
+console.log(isPrime(277473));
